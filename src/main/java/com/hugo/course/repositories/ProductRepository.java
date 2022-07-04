02325a6644840e5a4f29c787535707e3bc42483c
0MiniPorrinha <hugorodrigues969@gmail.com>
2022-07-04 11:20:57 -0300

    Product

---
+++ src/main/java/com/hugo/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.hugo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hugo.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    
+}
