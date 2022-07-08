61e5103a7600d47549ac45e3d6acb0ba5baa8cdd
0MiniPorrinha <hugorodrigues969@gmail.com>
2022-07-08 11:40:45 -0300

    Add insert, delete and update in product with Exception handling

--- src/main/java/com/hugo/course/services/ProductService.java
+++ src/main/java/com/hugo/course/services/ProductService.java
@@ -3,11 +3,17 @@ package com.hugo.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.hugo.course.entities.Product;
 import com.hugo.course.repositories.ProductRepository;
+import com.hugo.course.services.exceptions.DatabaseException;
+import com.hugo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -21,7 +27,35 @@ public class ProductService {
 
     public Product findById(Long id){
         Optional<Product> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     
+    public Product insert(Product product){
+        return repository.save(product);
+    }
+
+    public void delete(Long id){
+        try{
+            repository.deleteById(id);
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+    public Product update(Long id, Product product){
+        try{
+            Product entity = repository.getReferenceById(id);
+            updateData(entity, product);
+            return repository.save(entity);
+        }catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Product entity, Product product) {
+        entity.setName(product.getName());
+        entity.setDescription(product.getDescription());
+        entity.setPrice(product.getPrice());
+    }
 }
